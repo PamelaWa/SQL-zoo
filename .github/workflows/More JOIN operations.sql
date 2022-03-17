2a853c9eacee0d3209842d19d7ccdf8851a6be9a
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-17 13:16:47 -0400

    Update More JOIN operations.sql

--- .github/workflows/More JOIN operations.sql
+++ .github/workflows/More JOIN operations.sql
@@ -32,18 +32,43 @@ ON casting.actorid = actor.id
 WHERE casting.movieid = 11768
 
 --7.Obtain the cast list for the film 'Alien'
+SELECT actor.name
+FROM actor
+JOIN casting
+ON casting.actorid = actor.id
+JOIN movie
+ON movie.id = casting.movieid
+WHERE movie.title = 'Alien'
 
+--8.List the films in which 'Harrison Ford' has appeared
+SELECT movie.title
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON actor.id = casting.actorid
+WHERE actor.name = 'Harrison Ford'
 
+--9.List the films where 'Harrison Ford' has appeared - but not in the starring role. 
+--[Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+SELECT movie.title
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON actor.id = casting.actorid
+WHERE actor.name = 'Harrison Ford' AND ord <> 1
 
+--10.List the films together with the leading star for all 1962 films.
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON actor.id = casting.actorid
+WHERE ord = 1 AND yr = 1962
 
-
-
-
-
-
-
-
-
+--11.Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
 
 
 
