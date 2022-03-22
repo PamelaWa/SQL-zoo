aca1fc4479455f7ebeb11305604c79d26d70f457
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-22 11:39:16 -0400

    Update Using Null.sql

--- .github/workflows/Using Null.sql
+++ .github/workflows/Using Null.sql
@@ -31,9 +31,20 @@ SELECT COUNT(name) AS NOofT, COUNT(mobile) AS NOofM
 FROM teacher
 
 --8.Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name
 
+--*9.Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+SELECT teacher.name, CASE WHEN dept IN (1, 2) THEN 'Sci'
+ELSE 'Art' END 
+FROM teacher
 
-
+--10.Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+SELECT teacher.name, 
+CASE WHEN dept IN (1, 2) THEN 'Sci' 
+     WHEN dept = 3 THEN 'ART' ELSE 'None' END
+FROM teacher
 
 
 
