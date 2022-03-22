c4283e245367a355f3bc273b940f97babccd1410
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-22 11:06:42 -0400

    Update Using Null.sql

--- .github/workflows/Using Null.sql
+++ .github/workflows/Using Null.sql
@@ -23,10 +23,14 @@ SELECT name, COALESCE(mobile, '07986 444 2266')
 FROM teacher
 
 --6.Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON teacher.dept = dept.id
 
+--7.Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(name) AS NOofT, COUNT(mobile) AS NOofM
+FROM teacher
 
-
-
+--8.Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
 
 
 
