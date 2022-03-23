ba4f4d1328e7fc103e1812a8a6788d3df927f1b7
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-23 12:45:11 -0400

    Update Self join.sql

--- .github/workflows/Self join.sql
+++ .github/workflows/Self join.sql
@@ -51,11 +51,23 @@ WHERE stopa.name='Craiglockhart' AND stopb.name='Tollcross'
 
 --9.Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. 
 --Include the company and bus no. of the relevant services.
+SELECT  DISTINCT stopb.name, a.company, a.num 
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' 
 
-
-
-
-
+--10.Find the routes involving two buses that can go from Craiglockhart to Lochend.
+--Show the bus no. and company for the first bus, the name of the stop for the transfer,
+--and the bus no. and company for the second bus.
+SELECT a.num, a.company, stops.name, c.num, c.company
+from route a join route b on a.company=b.company AND a.num=b.num
+join stops on stops.id=a.stop
+join route c on stops.id=c.stop
+join route d on c.company=d.company AND c.num=d.num
+where b.stop =(select id from stops where name= 'Craiglockhart')
+and d.stop =(select id from stops where name= 'Lochend')
 
 
 
